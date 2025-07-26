df86390892a5feca888ac1ef9d4a19a5b2d7b91c
tady <tady0857@gmail.com>
2025-07-26 16:03:04 +0800

    傻眼==

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,33 +15,10 @@ import GoogleSignIn
     // Then register plugins
     GeneratedPluginRegistrant.register(with: self)
     
-    // Configure Google Sign-In
-    GIDSignIn.sharedInstance.clientID = FirebaseApp.app()?.options.clientID
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-  
-  @available(iOS 9.0, *)
-  override func application(_ application: UIApplication, open url: URL,
-                           options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     return GIDSignIn.sharedInstance.handle(url)
-  }
-  
-  // For iOS 8 and earlier
-  override func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-    return GIDSignIn.sharedInstance.handle(url)
-  }
-  
-  override func application(
-    _ app: UIApplication,
-    open url: URL,
-    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-  ) -> Bool {
-    // Handle Google Sign-In URL
-    if GIDSignIn.sharedInstance.handle(url) {
-      return true
-    }
-    
-    return super.application(app, open: url, options: options)
   }
 }
